607cf6db1730fd39bf3486906bb33f4903c93412
KolyaDavydov <kolya.davydov@inbox.ru>
2022-03-21 21:42:53 +0300

    added Task6 to matrix

---
+++ IntroductionToJavaOnline/src/algorithmization_2/arraysOfArrays/Task5.java
@@ -0,0 +1,25 @@
+package algorithmization_2.arraysOfArrays;
+
+//5. Сформировать квадратную матрицу порядка n по заданному образцу(n - четное)
+
+public class Task5 {
+	
+	public static void main(String[] args) {
+		int n = 8;
+		int[][] matrix = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n - i; j++) {
+				matrix[i][j] = i + 1;
+				
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(" " + matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
