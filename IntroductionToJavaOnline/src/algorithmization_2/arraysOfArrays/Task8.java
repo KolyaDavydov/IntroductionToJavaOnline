8d7a45770bbabe53794e7dee33eae069e029fb88
KolyaDavydov <kolya.davydov@inbox.ru>
2022-03-22 22:40:57 +0300

    added Task9 and fixed Task8 in matrix

--- IntroductionToJavaOnline/src/algorithmization_2/arraysOfArrays/Task8.java
+++ IntroductionToJavaOnline/src/algorithmization_2/arraysOfArrays/Task8.java
@@ -1,39 +1,52 @@
 package algorithmization_2.arraysOfArrays;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task8 {
 
 	public static void main(String[] args) {
-		int n = 6;
+		int n = 8;
 		int[][] matrix = new int[n][n];
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-				matrix[i][j] = (int) (Math.random() * 10);
+				matrix[i][j] = j + 1;
+
 			}
 		}
+
+		System.out.println("Имеется исходный массив:");
 		for(int i = 0; i < n; i++) {
-			System.out.println(Arrays.toString(matrix[i]));
+			for(int j = 0; j < n; j++) {
+				System.out.print(" " + matrix[i][j] + " ");
+			}
+			System.out.println();
 		}
-		
-		int numOfColumn = 0;
-		int sumMax = 0;
-		for(int j = 0; j < n; j++) {
-			int summ = 0;
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Введите два номера столбцов которые хотите поиенять от 1 до " + n);
+			int replaceFirst = scanner.nextInt();
+			int replaceSecond = scanner.nextInt();
+
+			int[][] matrixReplace = new int[n][n];
 			for(int i = 0; i < n; i++) {
-				summ = summ + matrix[i][j];
+				for(int j = 0; j < n; j++) {
+					matrixReplace[i][j] = matrix[i][j];
+					matrixReplace[i][replaceFirst - 1] = matrix[i][replaceSecond - 1];
+					matrixReplace[i][replaceSecond - 1] = matrix[i][replaceFirst - 1];
+				}
 			}
-			
-			if(summ > sumMax) {
-				sumMax = summ;
-				numOfColumn = j + 1;
+
+			System.out.println("Выводим Массив после перестановок столбцов:");
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.print(" " + matrixReplace[i][j] + " ");
+				}
+				System.out.println();
 			}
-			
 		}
-		
-		System.out.println("\nСтобец под номером " + numOfColumn + " имеет наибольшую сумму элементов равную: " + sumMax);
 
 	}
 
 }
+
 
